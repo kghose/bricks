d1b92c0aff2eb514f9f525fc0e574800555622f8
Kaushik Ghose <kaushik.ghose@gmail.com>
2022-03-03 19:38:54 -0500

    Redesign code to operate in star fashion.
    Now all conversions go to and from the
    standard CSV format.

--- Readme.md
+++ Readme.md
@@ -1,6 +1,45 @@
 # Simple scripts for manipulating LEGO part lists
 
-## owl2link
+```
+./bricks.py -h
+
+./bricks.py from_owl brickowl_order_items.csv
+./bricks.py to_bl brickowl_order_items.bricks
+```
+
+## Operations
+1. Save BrickOwl or BrickLink purchases to an inventory of parts I have.
+1. Convert a BrickOwl wishlist to a BrickLink wishlist.
+1. Create a Stud.io palette from an inventory.
+1. Subtract out parts used up in a build from inventory
+1. Create a wishlist of low stock parts 
+
+
+## Standardized CSV format
+
+```
+part,color,quantity
+```
+
+For internal manipulations I use a standardized CSV format with these columns
+which matches the format that ReBrickable can import. The color code is what
+ReBrickable uses which is the same as BrickLink.
+
+A given file can stand for my inventory or the quantity needed for a model and
+can be used to do subtractions and additions. 
+
+## Naming convention
+
+The program uses file names as metadata. The basename part of the file name is
+kept fixed while the trailing part is altered based on what format it is in.
+
+```
+<prefix>.csv - BO export
+<prefix>.brick - standard format, also readable by rebrickable
+<prefix>.xml - Importable into Stud.io and BrickLink
+```
+
+## Appendix I
 
 `./owl2link.py brickowl_order_items.csv bricklink_studio.xml`
 
@@ -18,14 +57,26 @@ The challenges in the conversion are
 1. Converting the BO color name to a code BrickLink Stud.io understands
 
 
+### Part registry
+
+The parts list ([rebrickable-parts.csv](rebrickable-parts.csv)) is obtained from
+the [Rebrickable downloads section](https://rebrickable.com/downloads/)
+(parts.csv.gz). 
+
+Some parts numbers are either missing from `parts.csv,gz` or BrickLink Stud.io uses a
+different part number than what is found in that file. These parts are mapped by
+hand in [missing-parts.csv](missing-parts.csv)
+
+### Color mapping
+
 For the BO vs BL color name/id, [this page](https://rebrickable.com/colors/) is
 useful. I used Google Sheets `importhtml` function
 (`importhtml("https://rebrickable.com/colors/", "table")`) to create a
 spreadsheet and then a CSV with the relevant mapping. Thanks to the tip
 [here](https://stackoverflow.com/questions/259091/how-can-i-scrape-an-html-table-to-csv/28083469#28083469) 
+This table is stored [rebrickable-colormapping.csv](rebrickable-colormapping.csv)
 
-
-## Appendix
+## Appendix II
 
 BrickOwl CSV format:
 ```
--- owl2link.py
+++ owl2link.py
@@ -1,19 +1,28 @@
 #!/usr/local/bin/python3
 
-"""Convert a BrickOwl parts CSV to a BrickLink XML that can be imported into
-BrickLink Studio
-
-usage:
-
-./owl2link.py brickowl_order_items.csv bricklink_studio.xml
-
-"""
+"""Interconvert parts lists to/from BrickOwl, Stud.io, BrickLink and ReBrickable
+accepted formats."""
 
 import argparse
+import csv
 from dataclasses import dataclass
 import sys
 from pathlib import Path
-import csv
+from typing import Dict, List
+
+
+class PartsPath:
+    def __init__(self, file_path: Path) -> None:
+        self._path = file_path
+
+    def as_bo(self) -> Path:
+        return self._path.with_suffix(".csv")
+
+    def as_std(self) -> Path:
+        return self._path.with_suffix(".bricks")
+
+    def as_bl(self) -> Path:
+        return self._path.with_suffix(".xml")
 
 
 @dataclass
@@ -39,13 +48,40 @@ class Color:
         return f"{self.bo_str} -> {self.bl_id}"
 
 
+class ColorConverter:
+    def __init__(self) -> None:
+        self._bo_name = {}
+        self._bo_id = {}
+        self._bl_id = {}
+        with open("rebrickable-colormapping.csv") as color_csv:
+            reader = csv.DictReader(color_csv)
+            for row in reader:
+                if row["BrickOwl"] and row["BrickLink"]:
+                    col = Color.from_str(row["BrickOwl"], row["BrickLink"])
+                    self._bo_name[col.bo_str.lower()] = col
+                    self._bo_id[col.bo_id] = col
+                    self._bl_id[col.bl_id] = col
+
+    def from_bo_str(self, bo: str) -> Color:
+        return self._bo_name[bo.lower()]
+
+    def from_bo_id(self, bo_id: int) -> Color:
+        return self._bo_id[bo_id]
+
+    def from_bl_id(self, bl_id: int) -> Color:
+        return self._bl_id[bl_id]
+
+
+color_converter = ColorConverter()
+
+
 @dataclass
 class Part:
     part_no: str
     color: Color
     qty: int
 
-    def as_brick_link(self, wishlist: bool = False, ignore_color: bool = False):
+    def as_brick_link(self, as_wishlist: bool = False, ignore_color: bool = False):
         return "".join(
             [
                 "<ITEM><ITEMTYPE>P</ITEMTYPE>",
@@ -53,12 +89,15 @@ class Part:
                 ("" if ignore_color else f"<COLOR>{self.color.bl_id}</COLOR>"),
                 (
                     f"<MINQTY>{self.qty}</MINQTY>"
-                    if wishlist
+                    if as_wishlist
                     else "<QTYFILLED>{self.qty}</QTYFILLED>"
                 ),
                 "</ITEM>\n",
             ]
         )
+
+    def as_rebrickable(self):
+        return f"{self.part_no},{self.color.bl_id},{self.qty}\n"
 
     def __eq__(self, __o: "Part") -> bool:
         return self.part_no == __o.part_no and self.color.bl_id == __o.color.bl_id
@@ -71,24 +110,15 @@ class BrickOwl:
     def __init__(self) -> None:
         self._color_map = {}
         self._catalog = set()
-        self._load_color_map()
         self._load_catalog()
         self._load_missing_parts()
 
     def part_from_row(self, name: str, color: str, quantity: str) -> Part:
         return Part(
             self._part_no_from_name(name),
-            self._color_map.get(color.lower()),
+            color_converter.from_bo_str(color),
             int(quantity),
         )
-
-    def _load_color_map(self):
-        with open("rebrickable-colormapping.csv") as color_csv:
-            reader = csv.DictReader(color_csv)
-            for row in reader:
-                if row["BrickOwl"] and row["BrickLink"]:
-                    col = Color.from_str(row["BrickOwl"], row["BrickLink"])
-                    self._color_map[col.bo_str.lower()] = col
 
     def _load_catalog(self) -> set:
         with open("rebrickable-parts.csv") as catalog_csv:
@@ -115,11 +145,8 @@ class BrickOwl:
             return _part_no
 
 
-def main(
-    infile: Path, outfile: Path, wishlist: bool = False, ignore_color: bool = False
-):
+def load_bo(infile: Path) -> Dict[Part, Part]:
     bo = BrickOwl()
-
     parts = {}
     with open(infile) as brick_owl_csv:
         reader = csv.DictReader(brick_owl_csv)
@@ -133,42 +160,103 @@ def main(
                 parts[this_part] = this_part
             else:
                 parts[this_part].qty += this_part.qty
+    return parts
 
+
+def load_std(infile: Path) -> Dict[Part, Part]:
+    parts = {}
+    with open(infile) as std_csv:
+        reader = csv.DictReader(std_csv)
+        for row in reader:
+            this_part = Part(
+                part_no=row["part"],
+                color=color_converter.from_bl_id(int(row["color"])),
+                qty=int(row["quantity"]),
+            )
+            if this_part not in parts:
+                parts[this_part] = this_part
+            else:
+                parts[this_part].qty += this_part.qty
+    return parts
+
+
+def to_studio(
+    outfile: Path,
+    parts: Dict[Part, Part],
+    as_wishlist: bool = False,
+    ignore_color: bool = False,
+) -> None:
     with outfile.open("w") as studio_xml:
         studio_xml.write("<INVENTORY>\n")
         studio_xml.writelines(
-            part.as_brick_link(wishlist, ignore_color) for part in parts.values()
+            part.as_brick_link(as_wishlist, ignore_color) for part in parts.values()
         )
         studio_xml.write("</INVENTORY>\n")
 
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="BrickOwl CSV to BrickLink XML")
-    parser.add_argument(
+def to_std(
+    outfile: Path,
+    parts: Dict[Part, Part],
+) -> None:
+    with outfile.open("w") as std_csv:
+        std_csv.write("part,color,quantity\n")
+        std_csv.writelines(part.as_rebrickable() for part in parts.values())
+
+
+def owl2std(infile: Path) -> None:
+    path = PartsPath(infile)
+    parts = load_bo(path.as_bo())
+    to_std(path.as_std(), parts)
+
+
+def std2studio(
+    infile: Path, as_wishlist: bool = False, ignore_color: bool = False
+) -> None:
+    path = PartsPath(infile)
+    parts = load_std(path.as_std())
+    to_studio(path.as_bl(), parts, as_wishlist=as_wishlist, ignore_color=ignore_color)
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Parts Juggler")
+    subparsers = parser.add_subparsers(title="Sub Commands")
+
+    parser_from_bo = subparsers.add_parser(
+        "from_owl", help="BrickOwl CSV to Standard CSV."
+    )
+    parser_from_bo.add_argument(
         "owl",
         type=str,
         help="BrickOwl CSV",
     )
-    parser.add_argument(
-        "link",
+    parser_from_bo.set_defaults(func=lambda args: owl2std(Path(args.owl)))
+
+    parser_to_bl = subparsers.add_parser("to_bl", help="Standard CSV to BrickLink XML")
+    parser_to_bl.add_argument(
+        "std",
         type=str,
-        help="BrickLink XML to write",
+        help="Std CSV",
     )
-    parser.add_argument(
-        "--wishlist",
+    parser_to_bl.add_argument(
+        "--as-wishlist",
         action="store_true",
         help="Write as BL wishlist XML (rather than Stud.io part list)",
     )
-    parser.add_argument(
+    parser_to_bl.add_argument(
         "--ignore-color",
         action="store_true",
         help="Ignore colors",
     )
-    args = parser.parse_args()
-
-    main(
-        Path(args.owl),
-        Path(args.link),
-        wishlist=args.wishlist,
-        ignore_color=args.ignore_color,
+    parser_to_bl.set_defaults(
+        func=lambda args: std2studio(
+            Path(args.std), as_wishlist=args.as_wishlist, ignore_color=args.ignore_color
+        )
     )
+
+    args = parser.parse_args()
+    args.func(args)
+
+
+if __name__ == "__main__":
+
+    main()
